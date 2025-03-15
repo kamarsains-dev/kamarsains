1379383dde39c3da658d005dcdeaccfc4614cd5c
kamarsains-dev <kamarsainsdeveloper@gmail.com>
2025-03-15 14:50:18 +0700

    delete not-found.tsx

--- app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-2xl font-semibold text-gray-700">404 | Page Not Found</h1>
-    </div>
-  );
-}
