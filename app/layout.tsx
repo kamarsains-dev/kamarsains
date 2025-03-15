465cc0b935d1d49d66b0343a8f33e9053ea9e1b1
kamarsains-dev <kamarsainsdeveloper@gmail.com>
2025-03-15 14:18:04 +0700

    fix clerk key

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,6 @@ import { Space_Grotesk } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
-const clerkPubKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-if (!clerkPubKey) {
-  throw new Error("Missing Clerk publishable key");
-}
 
 const font = Space_Grotesk({
   variable: "--font-spacegrotesk",
@@ -27,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider publishableKey={clerkPubKey}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <html lang="en">
       <body
         className={font.className}
