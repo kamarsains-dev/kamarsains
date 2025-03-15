ea4f0a69889eb062dcd3b63cbb410af9ef4410f0
kamarsains-dev <kamarsainsdeveloper@gmail.com>
2025-03-15 14:40:31 +0700

    KAPAN SELESAI

---
+++ app/(main)/start/page.tsx
@@ -0,0 +1,11 @@
+const Start = () => {
+    return (
+        <div className="w-full flex justify-center items-center text-center">
+            <h1>
+                Anda sudah login
+            </h1>
+        </div>
+    );
+};
+
+export default Start;
